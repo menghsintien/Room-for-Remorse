ad17eb8d5da77df9d5596aa9b67c720e80be0616
MengHsin <menghsintien0221@gmail.com>
2017-12-12 22:17:27 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,285 @@
+$(function () {
+
+    //==========================================
+    //detect
+    $('.anim1').waypoint(function () {
+        
+        
+        //todo 
+        $('.anim1').find("#picsize") //find the image
+        
+            .addClass('animated fadeInUp')
+            .css("visibility", "visible");
+        
+        
+    }, {
+        offset: '100%'
+    });
+    //==========================================
+    
+    
+});
+
+$(function () {
+
+    //==========================================
+    //detect
+    $('.anim2').waypoint(function () {
+        
+        
+        //todo 
+        $('.anim2').find("#picsize") //find the image
+        
+            .addClass('animated fadeInUp')
+            .css("visibility", "visible");
+        
+        
+    }, {
+        offset: '100%'
+    });
+    //==========================================
+    
+    
+});
+
+$(function () {
+
+    //==========================================
+    //detect
+    $('.anim3').waypoint(function () {
+        
+        
+        //todo 
+        $('.anim3').find("#picsize") //find the image
+        
+            .addClass('animated fadeInUp')
+            .css("visibility", "visible");
+        
+        
+    }, {
+        offset: '100%'
+    });
+    //==========================================
+    
+    
+});
+
+$(function () {
+
+    //==========================================
+    //detect
+    $('.anim4').waypoint(function () {
+        
+        
+        //todo 
+        $('.anim4').find("#picsize") //find the image
+        
+            .addClass('animated fadeInUp')
+            .css("visibility", "visible");
+        
+        
+    }, {
+        offset: '100%'
+    });
+    //==========================================
+    
+    
+});
+
+$(function () {
+
+    //==========================================
+    //detect
+    $('.anim5').waypoint(function () {
+        
+        
+        //todo 
+        $('.anim5').find("#picsize") //find the image
+        
+            .addClass('animated fadeInUp')
+            .css("visibility", "visible");
+        
+        
+    }, {
+        offset: '100%'
+    });
+    //==========================================
+    
+    
+});
+
+$(function () {
+
+    //==========================================
+    //detect
+    $('.anim6').waypoint(function () {
+        
+        
+        //todo 
+        $('.anim6').find("#picsize") //find the image
+        
+            .addClass('animated fadeInUp')
+            .css("visibility", "visible");
+        
+        
+    }, {
+        offset: '100%'
+    });
+    //==========================================
+    
+    
+});
+
+$(function () {
+
+    //==========================================
+    //detect
+    $('.anim7').waypoint(function () {
+        
+        
+        //todo 
+        $('.anim7').find("#picsize") //find the image
+        
+            .addClass('animated fadeInUp')
+            .css("visibility", "visible");
+        
+        
+    }, {
+        offset: '100%'
+    });
+    //==========================================
+    
+    
+});
+
+$(function () {
+
+    //==========================================
+    //detect
+    $('.anim8').waypoint(function () {
+        
+        
+        //todo 
+        $('.anim8').find("#picsize") //find the image
+        
+            .addClass('animated fadeInUp')
+            .css("visibility", "visible");
+        
+        
+    }, {
+        offset: '100%'
+    });
+    //==========================================
+    
+    
+});
+
+$(function () {
+
+    //==========================================
+    //detect
+    $('.anim9').waypoint(function () {
+        
+        
+        //todo 
+        $('.anim9').find("#picsize") //find the image
+        
+            .addClass('animated fadeInUp')
+            .css("visibility", "visible");
+        
+        
+    }, {
+        offset: '100%'
+    });
+    //==========================================
+    
+    
+});
+
+$(function () {
+
+    //==========================================
+    //detect
+    $('.anim10').waypoint(function () {
+        
+        
+        //todo 
+        $('.anim10').find("#picsize") //find the image
+        
+            .addClass('animated fadeInUp')
+            .css("visibility", "visible");
+        
+        
+    }, {
+        offset: '100%'
+    });
+    //==========================================
+    
+    
+});
+
+$(function () {
+
+    //==========================================
+    //detect
+    $('.anim11').waypoint(function () {
+        
+        
+        //todo 
+        $('.anim11').find("#picsize") //find the image
+        
+            .addClass('animated fadeInUp')
+            .css("visibility", "visible");
+        
+        
+    }, {
+        offset: '100%'
+    });
+    //==========================================
+    
+    
+});
+
+$(function () {
+
+    //==========================================
+    //detect
+    $('.anim12').waypoint(function () {
+        
+        
+        //todo 
+        $('.anim12').find("#picsize") //find the image
+        
+            .addClass('animated fadeInUp')
+            .css("visibility", "visible");
+        
+        
+    }, {
+        offset: '100%'
+    });
+    //==========================================
+    
+    
+});
+
+$(function () {
+
+    //==========================================
+    //detect
+    $('.anim13').waypoint(function () {
+        
+        
+        //todo 
+        $('.anim13').find("#picsize") //find the image
+        
+            .addClass('animated fadeInUp')
+            .css("visibility", "visible");
+        
+        
+    }, {
+        offset: '100%'
+    });
+    //==========================================
+    
+    
+});
